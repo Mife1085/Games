c0d549c8492af6de8605d5d9898158722cd88bb1
Sergey Digin <56755919+Mife1085@users.noreply.github.com>
2024-03-13 07:16:23 +0500

    Update Items.py

--- Items.py
+++ Items.py
@@ -1,270 +1,284 @@
-# Добавить id предметам 
 Items = {
     "Weapons" : {
-        {
         "Swords": [
             {
                 "Name": "Деревянный Меч",
                 "Damage": "2-4",
                 "Speed": "8f",
-                "Type": "Режущий/Одноручный"
-                "Scale" : "D Сила"
+                "Type": "Режущий/Одноручный",
+
+                "Equipment" : "Disable"
+                # "Scale" : "D Сила"
             
             },
             {
                 "Name": "Бронзовый Меч",
                 "Damage": "1-3",
                 "Speed": "17f",
-                "Type": "Режущий/Одноручный"
+                "Type": "Режущий/Одноручный",
+                "Equipment" : "Disable"
             
             },
             {
                 "Name": "Железный Меч",
                 "Damage": "2-4",
                 "Speed": "6f",
-                "Type": "Режущий/Одноручный"
+                "Type": "Режущий/Одноручный",
+                "Equipment" : "Disable"
             
             },
             {
                 "Name": "Стальной Меч",
                 "Damage": "3-5",
                 "Speed": "22f",
-                "Type": "Режущий/Одноручный"
+                "Type": "Режущий/Одноручный",
+                "Equipment" : "Disable"
             
             },
             {
                 "Name": "Титановый Меч",
                 "Damage": "4-6",
                 "Speed": "8f",
-                "Type": "Режущий/Одноручный"
+                "Type": "Режущий/Одноручный",
+                "Equipment" : "Disable"
            
-            }
-        ],
+            }],
+
         "Maces": [
             {
                 "Name": "Деревянная Дубина",
                 "Damage": "1-5",
                 "Speed": "10f",
-                "Type": "Дробящий/Одноручная"
-                "Scale" : "C Сила"
+                "Type": "Дробящий/Одноручная",
+
+                "Equipment" : "Disable"                # "Scale" : "C Сила"
             },
             {
                 "Name": "Бронзовая Дубина",
                 "Damage": "1-3",
                 "Speed": "21f",
-                "Type": "Дробящий/Одноручная"
-            
+                "Type": "Дробящий/Одноручная",
+                "Equipment" : "Disable"            
             },
             {
                 "Name": "Железная Дубина",
                 "Damage": "2-4",
                 "Speed": "33f",
-                "Type": "Дробящий/Одноручная"
-            
+                "Type": "Дробящий/Одноручная",
+                "Equipment" : "Disable"            
             },
             {
                 "Name": "Стальная Дубина",
                 "Damage": "3-5",
                 "Speed": "8f",
-                "Type": "Дробящий/Одноручная"
-            
+                "Type": "Дробящий/Одноручная",
+                "Equipment" : "Disable"            
             },
             {
                 "Name": "Титановая Дубина",
                 "Damage": "4-6",
                 "Speed": "16f",
-                "Type": "Дробящий/Одноручная"
-           
-            }
-        ],
+                "Type": "Дробящий/Одноручная",
+                "Equipment" : "Disable"           
+            }],
+
         "Spears": [
             {
                 "Name": "Деревянное Копьё",
                 "Damage": "3-7",
                 "Speed": "14f",
-                "Type": "Колучиший/Двухручное"
-                "Scale" : "D Сила, D Ловкость"
+                "Type": "Колучиший/Двухручное",
+
+                "Equipment" : "Disable"                # "Scale" : "D Сила, D Ловкость"
             
             },
             {
                 "Name": "Бронзовое Копьё",
                 "Damage": "1-3",
                 "Speed": "38f",
-                "Type": "Колучиший/Двухручное"
-            
+                "Type": "Колучиший/Двухручное",
+                "Equipment" : "Disable"            
             },
             {
                 "Name": "Железное Копьё",
                 "Damage": "2-4",
                 "Speed": "23f",
-                "Type": "Колучиший/Двухручное"
-            
+                "Type": "Колучиший/Двухручное",
+                "Equipment" : "Disable"            
             },
             {
                 "Name": "Стальное Копьё",
                 "Damage": "3-5",
                 "Speed": "18f",
-                "Type": "Колучиший/Двухручное"
-            
+                "Type": "Колучиший/Двухручное",
+                "Equipment" : "Disable"            
             },
             {
                 "Name": "Титановое Копьё",
                 "Damage": "4-6",
                 "Speed": "28f",
-                "Type": "Колучиший/Двухручное"
-           
-            }
-        ],
+                "Type": "Колучиший/Двухручное",
+                "Equipment" : "Disable"           
+            }],
+
         "Axes": [
             {
                 "Name": "Деревянный Топор",
                 "Damage": "2-3",
                 "Speed": "7f",
-                "Type": "Режущий/Одноручный"
-                "Scale" : "С Сила"
+                "Type": "Режущий/Одноручный",
+
+                "Equipment" : "Disable"                # "Scale" : "С Сила"
             },
             {
                 "Name": "Бронзовый Топор",
                 "Damage": "1-3",
                 "Speed": "15f",
-                "Type": "Режущий/Одноручный"
-            
+                "Type": "Режущий/Одноручный",
+                "Equipment" : "Disable"            
             },
             {
                 "Name": "Железный Топор",
                 "Damage": "2-4",
                 "Speed": "5f",
-                "Type": "Режущий/Одноручный"
-            
+                "Type": "Режущий/Одноручный",
+                "Equipment" : "Disable"            
             },
             {
                 "Name": "Стальный Топор",
                 "Damage": "3-5",
                 "Speed": "39f",
-                "Type": "Режущий/Одноручный"
-            
+                "Type": "Режущий/Одноручный",
+                "Equipment" : "Disable"            
             },
             {
                 "Name": "Титановый Топор",
                 "Damage": "4-6",
                 "Speed": "21f",
-                "Type": "Режущий/Одноручный"
-           
-            }
-        ],
+                "Type": "Режущий/Одноручный",
+                "Equipment" : "Disable"           
+            }],
+
         "Knifes": [
             {
                 "Name": "Деревянный Нож",
                 "Damage": "1-2",
                 "Speed": "4f",
-                "Type": "Колучиший/Одноручный"
-                "Scale" : "B Ловкость"
+                "Type": "Колучиший/Одноручный",
+
+                "Equipment" : "Disable"                # "Scale" : "B Ловкость"
             },
             {
                 "Name": "Бронзовый Нож",
                 "Damage": "1-3",
                 "Speed": "6f",
-                "Type": "Колучиший/Одноручный"
-            
+                "Type": "Колучиший/Одноручный",
+                "Equipment" : "Disable"            
             },
             {
                 "Name": "Железный Нож",
                 "Damage": "2-4",
                 "Speed": "11f",
-                "Type": "Колучиший/Одноручный"
-            
+                "Type": "Колучиший/Одноручный",
+                "Equipment" : "Disable"            
             },
             {
                 "Name": "Стальный Нож",
                 "Damage": "3-5",
                 "Speed": "15f",
-                "Type": "Колучиший/Одноручный"
-            
+                "Type": "Колучиший/Одноручный",
+                "Equipment" : "Disable"            
             },
             {
                 "Name": "Титановый Нож",
                 "Damage": "4-6",
                 "Speed": "22f",
-                "Type": "Колучиший/Одноручный"
-           
-            }
-        ],
+                "Type": "Колучиший/Одноручный",
+                "Equipment" : "Disable"           
+            }],
+
         "Rapier": [
             {
                 "Name": "Деревянная Рапира",
                 "Damage": "1-4",
                 "Speed": "7f",
-                "Type": "Колучиший/Одноручная"
-                "Scale" : "C Ловкость"
+                "Type": "Колучиший/Одноручная",
+
+                "Equipment" : "Disable"                # "Scale" : "C Ловкость"
             },
             {
                 "Name": "Бронзовая Рапира",
                 "Damage": "1-3",
                 "Speed": "37f",
-                "Type": "Колучиший/Одноручная"
-            
+                "Type": "Колучиший/Одноручная",
+                "Equipment" : "Disable"            
             },
             {
                 "Name": "Железная Рапира",
                 "Damage": "2-4",
                 "Speed": "9f",
-                "Type": "Колучиший/Одноручная"
-            
+                "Type": "Колучиший/Одноручная",
+                "Equipment" : "Disable"            
             },
             {
                 "Name": "Стальная Рапира",
                 "Damage": "3-5",
                 "Speed": "34f",
-                "Type": "Колучиший/Одноручная"
-            
+                "Type": "Колучиший/Одноручная",
+                "Equipment" : "Disable"            
             },
             {
                 "Name": "Титановая Рапира",
                 "Damage": "4-6",
                 "Speed": "6f",
-                "Type": "Колучиший/Одноручная"
-           
-            }
-        ],
+                "Type": "Колучиший/Одноручная",
+                "Equipment" : "Disable"           
+            }],
+
         "Bow": [
             {
                 "Name": "Деревянный Лук",
                 "Damage": "7-9",
                 "Speed": "18f",
-                "Type": "Дальний бой/Двухручный"
-                "Scale" : "D Ловкость"
+                "Type": "Дальний бой/Двухручный",
+
+                "Equipment" : "Disable"
+                # "Scale" : "D Ловкость"
             
             },
             {
                 "Name": "Бронзовый Лук",
                 "Damage": "1-3",
                 "Speed": "12f",
-                "Type": "Дальний бой/Двухручный"
+                "Type": "Дальний бой/Двухручный",
+                "Equipment" : "Disable"
             
             },
             {
                 "Name": "Железный Лук",
                 "Damage": "2-4",
                 "Speed": "15f",
-                "Type": "Дальний бой/Двухручный"
+                "Type": "Дальний бой/Двухручный",
+                "Equipment" : "Disable"
             
             },
             {
                 "Name": "Стальной Лук",
                 "Damage": "3-5",
                 "Speed": "12f",
-                "Type": "Дальний бой/Двухручный"
+                "Type": "Дальний бой/Двухручный",
+                "Equipment" : "Disable"
             
             },
             {
                 "Name": "Титановый Лук",
                 "Damage": "4-6",
                 "Speed": "21f",
-                "Type": "Дальний бой/Двухручный"
+                "Type": "Дальний бой/Двухручный",
+                "Equipment" : "Disable"
            
-            }
-    ]
-}
+            }]
+    },
     
     "Armor" : {
         "Shelds" : [], 
@@ -285,14 +299,18 @@ Items = {
         "Rings" : [], 
         "Amulets" : []
     } 
- } }
+ } 
  
-import random
-Materials = ["Деревянн", "Бронзов", "Железн", "Стальн", "Титанов"]
-for item in Items["Weapons"]:
-    i = 0
-    while i < len(Materials):
-        Items["Weapons"][item].append({"Name" : f"{Materials[i]} {item}", "Damage" : f"{i}-{i+2}", "Speed" : f"{random.randint(5, 40)}f", "Type" : "Колучиший"})
-        i +=
-         1
-print(Items["Weapons"])
+# import random
+# Materials = ["Деревянн", "Бронзов", "Железн", "Стальн", "Титанов"]
+# for item in Items["Weapons"]:
+#     i = 0
+#     while i < len(Materials):
+#         Items["Weapons"][item].append({"Name" : f"{Materials[i]} {item}", "Damage" : f"{i}-{i+2}", "Speed" : f"{random.randint(5, 40)}f", "Type" : "Колучиший"})
+#         i += 1
+# print(Items["Weapons"])
+id_list = []
+for category in Items:
+    for Item_type in Items[category]:
+        for Item in Items[category][Item_type]:
+            id_list.append(Item)
