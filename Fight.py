01f5f90ff74abc5ddb1e1e0053e7b6f444246f68
Sergey Digin <56755919+Mife1085@users.noreply.github.com>
2024-03-13 07:16:59 +0500

    Update Fight.py

--- Fight.py
+++ Fight.py
@@ -1,4 +1,5 @@
 import random
+import Items
 player1 = {
 "Player" : {
         "Name": "Mife",
@@ -10,11 +11,10 @@ player1 = {
                 "Stats" : [4, 3, 1, 2, 0, 0] # Жиз, Сил, Лов, Вын, Инт, Удч
         },
 
-        "Inventory" : {
-                "Нож" : {"Урон": "2-4 урона", "Скорость" : "4f" , "scale" : "AC", "Equipment" : "Enable"},
-                "Кожаная куртка" : {"Защита" : "2 Зщт", "Equipment" : "Enable"},
+        "Inventory" : [
+                Items.id_list[0], Items.id_list[2], Items.id_list[5], Items.id_list[23] 
 
-        },
+        ],
 
         "Info" : {}
         }
@@ -30,7 +30,9 @@ player2 = {
                 "Gold" : 0,
                 "Stats" : [4, 2, 4, 0, 0, 0]
         },
-        "Inventory" : {"Кожаная куртка" : {"Защита" : "2 Зщт", "Equipment" : "Enable"},},
+        "Inventory" : [
+                Items.id_list[0]
+        ],
 
         "Info" : {}
         }
@@ -63,10 +65,10 @@ def Fight(player1, player2):
                 PDefens = PDefens
                 EDefens = EDefens
 
-                for val in player2["Player"]["Inventory"]:
-                        if player2["Player"]["Inventory"][val].get("Equipment") == "Enable":
-                                if player2["Player"]["Inventory"][val].get("Урон") != None:
-                                        EDamage = player2["Player"]["Statistics"]["Stats"][1] + random.randint(int(player2["Player"]["Inventory"][val].get("Урон")[0]), int(player2["Player"]["Inventory"][val].get("Урон")[2]))
+                for item in player2["Player"]["Inventory"]:
+                        if item.get("Equipment") == "Enable":
+                                if player2["Player"]["Inventory"][item].get("Damage") != None:
+                                        EDamage = player2["Player"]["Statistics"]["Stats"][1] + random.randint(int(player2["Player"]["Inventory"][item].get("Damage")[0]), int(player2["Player"]["Inventory"][item].get("Damage")[2]))
                                 else:
                                         EDamage = player2["Player"]["Statistics"]["Stats"][1]
 
@@ -87,27 +89,28 @@ def Fight(player1, player2):
                 return PDefens
 
         def Get_Damage_and_Defens(param):
-                if param == "PDmg":
-                        for val in player1["Player"]["Inventory"]:
-                                if player1["Player"]["Inventory"][val].get("Equipment") == "Enable":
-                                        if player1["Player"]["Inventory"][val].get("Урон", None) != None:
-                                                return int((int(player1["Player"]["Inventory"][val].get("Урон")[0]) + int(player1["Player"]["Inventory"][val].get("Урон")[2]))/2)
+                
+                        for item in player1["Player"]["Inventory"]:
+                                if player1["Player"]["Inventory"][item].get("Equipment") == "Enable":
+                                                if param == "PDmg":
+                                        if player1["Player"]["Inventory"][item].get("Damage", None) != None:
+                                                return int((int(player1["Player"]["Inventory"][item].get("Damage")[0]) + int(player1["Player"]["Inventory"][item].get("Damage")[2]))/2)
                 if param == "PDef":                                                
-                        for val in player1["Player"]["Inventory"]:
-                                if player1["Player"]["Inventory"][val].get("Equipment") == "Enable":
-                                        if player1["Player"]["Inventory"][val].get("Защита") != None:
-                                                return int(player1["Player"]["Inventory"][val].get("Защита")[0])
+                        for item in player1["Player"]["Inventory"]:
+                                if player1["Player"]["Inventory"][item].get("Equipment") == "Enable":
+                                        if player1["Player"]["Inventory"][item].get("Защита") != None:
+                                                return int(player1["Player"]["Inventory"][item].get("Защита")[0])
 
                 if param == "EDmg":
-                        for val in player2["Player"]["Inventory"]:
-                                if player2["Player"]["Inventory"][val].get("Equipment") == "Enable":
-                                        if player2["Player"]["Inventory"][val].get("Урон", None) != None:
-                                                return int((int(player2["Player"]["Inventory"][val].get("Урон")[0]) + int(player2["Player"]["Inventory"][val].get("Урон")[2]))/2)
+                        for item in player2["Player"]["Inventory"]:
+                                if player2["Player"]["Inventory"][item].get("Equipment") == "Enable":
+                                        if player2["Player"]["Inventory"][item].get("Damage", None) != None:
+                                                return int((int(player2["Player"]["Inventory"][item].get("Damage")[0]) + int(player2["Player"]["Inventory"][item].get("Damage")[2]))/2)
                 if param == "EDef":                                                
-                        for val in player2["Player"]["Inventory"]:
-                                if player2["Player"]["Inventory"][val].get("Equipment") == "Enable":
-                                        if player2["Player"]["Inventory"][val].get("Защита") != None:
-                                                return int(player2["Player"]["Inventory"][val].get("Защита")[0]) 
+                        for item in player2["Player"]["Inventory"]:
+                                if player2["Player"]["Inventory"][item].get("Equipment") == "Enable":
+                                        if player2["Player"]["Inventory"][item].get("Защита") != None:
+                                                return int(player2["Player"]["Inventory"][item].get("Защита")[0]) 
 
         PDamage_const = Get_Damage_and_Defens("PDmg") + player1["Player"]["Statistics"]["Stats"][1]if Get_Damage_and_Defens("PDmg") != None else player1["Player"]["Statistics"]["Stats"][1]
         PDefens = Get_Damage_and_Defens("PDef") + player1["Player"]["Statistics"]["Stats"][4] if Get_Damage_and_Defens("PDef") != None else player1["Player"]["Statistics"]["Stats"][4]
@@ -165,10 +168,10 @@ HP: {player2["Player"]["Statistics"]["Hp"]} | Зщт {EDefens if EDefens > 0 els
                                 # При +0 фраймов. действие игрока Удар -> нож (7 фреймов) и Действии противника Удар Кувалдой (10 ф.) 
                                 # Число общих фреймов становиться -7. Игрок наносит удар и снова выбирает действие. Выбор падает на перекат в сторону (4).
                                 # Игрок не успевает и получат урон и минус по общим фреймам
-                                        for val in player1["Player"]["Inventory"]:
-                                                if player1["Player"]["Inventory"][val].get("Equipment") == "Enable":
-                                                        if player1["Player"]["Inventory"][val].get("Урон") != None:
-                                                                PDamage = player1["Player"]["Statistics"]["Stats"][1] + random.randint(int(player1["Player"]["Inventory"][val].get("Урон")[0]), int(player1["Player"]["Inventory"][val].get("Урон")[2]))
+                                        for item in player1["Player"]["Inventory"]:
+                                                if player1["Player"]["Inventory"][item].get("Equipment") == "Enable":
+                                                        if player1["Player"]["Inventory"][item].get("Damage") != None:
+                                                                PDamage = player1["Player"]["Statistics"]["Stats"][1] + random.randint(int(player1["Player"]["Inventory"][item].get("Damage")[0]), int(player1["Player"]["Inventory"][item].get("Damage")[2]))
                                         Hit = PDamage
                                         Enemy_escape = (player2["Player"]["Statistics"]["Stats"][2] - player1["Player"]["Statistics"]["Stats"][2])*10 if ((player2["Player"]["Statistics"]["Stats"][2] - player1["Player"]["Statistics"]["Stats"][2])*10)>=0 else 0
                                         if random.randint(0, 100) <= 100-Enemy_escape:
@@ -197,14 +200,14 @@ HP: {player2["Player"]["Statistics"]["Hp"]} | Зщт {EDefens if EDefens > 0 els
                                                 feature.append(inv[key2])
                                         print(objects, feature)
                                         print("Equipment: ")
-                                        for key, value in zip(objects, feature):
-                                                if value.get("Equipment") == "Enable":
-                                                        print(f"""{key} | {value.get("Урон", "")} {value.get("Defense", "")}""")
+                                        for key, itemue in zip(objects, feature):
+                                                if itemue.get("Equipment") == "Enable":
+                                                        print(f"""{key} | {itemue.get("Damage", "")} {itemue.get("Defense", "")}""")
                                                 
                                         print("Else: ")
-                                        for key, value in zip(objects, feature):
-                                                if value.get("Equipment", "Disabled") == "Disabled":
-                                                        print(f"""{key} | {value.get("Урон", "")} {value.get("Defense", "")}""")
+                                        for key, itemue in zip(objects, feature):
+                                                if itemue.get("Equipment", "Disabled") == "Disabled":
+                                                        print(f"""{key} | {itemue.get("Damage", "")} {itemue.get("Defense", "")}""")
 
                                 elif console == "4":
                                         Fight_now = False
